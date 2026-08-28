43ca8d03d3d06b9911b363b2de88647efcbaeabb
wsman217 <westonsublett1@hotmail.com>
2026-08-28 18:15:01 -0500

    Initial commit

---
+++ src/main/java/com/westonsublett/jobboard/JobBoardApplication.java
@@ -0,0 +1,12 @@
+package com.westonsublett.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobBoardApplication.class, args);
+    }
+}
